5c4d22dc5e53eb8dff90ad34ab689327e9c21a8e
Zifan Zhang <ziz012@ucsd.edu>
2020-09-11 20:41:11 +0800

    added homepage

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
+var fs = require('fs');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!\nWelcome!");
+    fs.readFile('homepage.html', function(err, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        return response.end();
+    });
 });
 
 const port = process.env.PORT || 1337;
