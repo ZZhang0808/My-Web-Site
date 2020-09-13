77ae8b9a1b3eda3dbe72bd52048a7ea9fa4f9ba1
Zifan Zhang <ziz012@ucsd.edu>
2020-09-13 20:21:11 +0800

    applied template

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 var fs = require('fs');
 
 const server = http.createServer((request, response) => {
-    fs.readFile('homepage.html', function(err, data) {
+    fs.readFile('App.html', function(err, data) {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         return response.end();
